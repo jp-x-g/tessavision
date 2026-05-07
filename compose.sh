af85d5cf3e959417a435d64c4ebbb28b373c88d3
jp-x-g <jpxg-dev@protonmail.com>
2026-05-06 22:40:25 -0700

    commit

--- compose.sh
+++ compose.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 date '+%A,%n%Y %b %d' | figlet -r -w 57 -f mini > blocks/0date.txt
-printf "Wednesday,\n2026 Www 30" | figlet -r -w 57 -f mini > blocks/0date.txt
+# printf "Wednesday,\n2026 Www 30" | figlet -r -w 57 -f mini > blocks/0date.txt
 # logo3.txt is 63 wide as of now. display is 120
 # paste blocks/logo3.txt blocks/3x11.txt blocks/3x11.txt blocks/0date.txt > blocks/header.txt
 paste blocks/logo3.txt blocks/0date.txt > blocks/header.txt

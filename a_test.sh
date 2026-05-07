01e31dbd39d887b77d68a2b36f8b347a033bd874
jp-x-g <jpxg-dev@protonmail.com>
2026-05-06 23:43:08 -0700

    commit

---
+++ a_test.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+date '+%A,%n%Y %b %d' | figlet -r -w 57 -f mini > blocks/0date.txt
+cat blocks/0date.txt
+
+echo "banner"
+date '+%A,%n%Y %b %d' | figlet -r -w 57 -f banner
+echo "big"
+date '+%A,%n%Y %b %d' | figlet -r -w 57 -f big
+echo "block"
+date '+%A,%n%Y %b %d' | figlet -r -w 57 -f block
+echo "bubble"
+date '+%A,%n%Y %b %d' | figlet -r -w 57 -f bubble
+echo "digital"
+date '+%A,%n%Y %b %d' | figlet -r -w 57 -f digital
+echo "ivrit"
+date '+%A,%n%Y %b %d' | figlet -r -w 57 -f ivrit
+echo "lean"
+date '+%A,%n%Y %b %d' | figlet -r -w 57 -f lean
+echo "mini"
+date '+%A,%n%Y %b %d' | figlet -r -w 57 -f mini
+echo "mnemonic"
+date '+%A,%n%Y %b %d' | figlet -r -w 57 -f mnemonic
+echo "script"
+date '+%A,%n%Y %b %d' | figlet -r -w 57 -f script
+echo "shadow"
+date '+%A,%n%Y %b %d' | figlet -r -w 57 -f shadow
+echo "slant"
+date '+%A,%n%Y %b %d' | figlet -r -w 57 -f slant
+echo "small"
+date '+%A,%n%Y %b %d' | figlet -r -w 57 -f small
+echo "smscript"
+date '+%A,%n%Y %b %d' | figlet -r -w 57 -f smscript
+echo "smshadow"
+date '+%A,%n%Y %b %d' | figlet -r -w 57 -f smshadow
+echo "smslant"
+date '+%A,%n%Y %b %d' | figlet -r -w 57 -f smslant
+echo "standard"
+date '+%A,%n%Y %b %d' | figlet -r -w 57 -f standard
+echo "term"
+date '+%A,%n%Y %b %d' | figlet -r -w 57 -f term
+
+
+
+
+# 69 plus date = 51 for it
+#clear
+cat blocks/header.txt
+sleep 15
+
+
+

9f1b7d81a0ab6db51dc33378b2e8389d13476076
jp-x-g <jpxg-dev@protonmail.com>
2026-05-06 20:09:23 -0700

    commit

--- compose.sh
+++ compose.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-date '+%A,%n%Y %b %d' > blocks/rawdate.txt
-figlet -f mini rawdate.txt > blocks/0date.txt
+date '+%A,%n%Y %b %d' | figlet -f mini > blocks/0date.txt
 paste blocks/logo3.txt blocks/0date.txt > blocks/header.txt
 cat blocks/header.txt

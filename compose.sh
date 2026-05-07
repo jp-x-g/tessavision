a67676f7d4238762b3ebe1b8c84cef3b93c6e3a1
jp-x-g <jpxg-dev@protonmail.com>
2026-05-06 20:14:23 -0700

    commit

--- compose.sh
+++ compose.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 date '+%A,%n%Y %b %d' | figlet -f mini > blocks/0date.txt
-date '+%A,%n%Y %b %d' | figlet -f mini "Wednesday,\
-2026 Oct 4" > blocks/0date.txt
+printf "Wednesday,\n2026 Oct 4" | figlet -f mini > blocks/0date.txt
 paste blocks/logo3.txt blocks/0date.txt > blocks/header.txt
+clear
 cat blocks/header.txt

0d93a2aca27702ed766cd461f56e9b60ae93ae72
jp-x-g <jpxg-dev@protonmail.com>
2026-05-06 17:23:48 -0700

    commit

--- startup.sh
+++ startup.sh
@@ -3,4 +3,7 @@
 sudo setterm --foreground yellow --bold on --background green --store
 sudo setfont Lat15-TerminusBold32x16.psf.gz
 clear
-echo "asdf"
+echo "Installing figlet."
+sudo apt-get -y update
+sudo apt-get -y install figlet
+sudo apt-get -y install fbi

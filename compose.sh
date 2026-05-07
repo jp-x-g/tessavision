8adbcf5be3a4bee70717e0e61602195d6da57c4d
jp-x-g <jpxg-dev@protonmail.com>
2026-05-07 00:01:55 -0700

    commit

--- compose.sh
+++ compose.sh
@@ -4,8 +4,9 @@
 # printf "Wednesday,\n2026 Www 30" | figlet -r -w 57 -f mini > blocks/0date.txt
 # logo3.txt is 63 wide as of now. display is 120
 # paste blocks/logo3.txt blocks/3x11.txt blocks/3x11.txt blocks/0date.txt > blocks/header.txt
-date '+%A,%n%Y %b %d' | figlet -r -w 55 -f smslant > blocks/0date.txt
-paste blocks/logo3.txt blocks/1x11.txt blocks/barx11.txt blocks/0date.txt > blocks/header.txt
+date '+%A,%n%Y %b %d' | figlet -r -w 56 -f smslant > blocks/0date.txt
+#paste blocks/logo3.txt blocks/1x11.txt blocks/barx11.txt blocks/0date.txt > blocks/header.txt
+paste blocks/logo3.txt blocks/0date.txt > blocks/header.txt
 # 69 plus date = 51 for it
 clear
 cat blocks/header.txt

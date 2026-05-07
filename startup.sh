c43cb1d5f12b2579d11f0a54a1884b76b6245063
jp-x-g <jpxg-dev@protonmail.com>
2026-05-06 23:54:42 -0700

    commit

--- startup.sh
+++ startup.sh
@@ -3,7 +3,10 @@
 sudo setterm --foreground yellow --bold on --background green --store
 sudo setfont Lat15-TerminusBold32x16.psf.gz
 clear
-echo "Installing figlet."
+sudo timedatectl set-timezone America/Los_Angeles
+echo "Updating apt-get."
 sudo apt-get -y update
+echo "Installing figlet."
 sudo apt-get -y install figlet
 sudo apt-get -y install fbi
+bash compose.sh

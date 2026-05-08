4db3f94559a6673508c52b191a870e113a90f145
jp-x-g <jpxg-dev@protonmail.com>
2026-05-07 18:12:37 -0700

    commit

--- ticker.sh
+++ ticker.sh
@@ -1,11 +1,27 @@
 #!/usr/bin/env bash
 
 APIKEY="$(tr -d '\n' < auth/financialmodelingprep.com.txt)"
+
+echo "CURLing SPY"
 curl -fsS \
   -A 'Mozilla/5.0 (X11; Linux x86_64; rv:136.0) Gecko/20100101 Firefox/136.0' \
   -H 'Accept: application/json,text/plain,*/*' \
   "https://financialmodelingprep.com/stable/quote-short?symbol=SPY&apikey=$APIKEY" \
-  | jq -r '.[0].price | floor' > temp/SP500.txt
+  | jq -r '.[0].price | floor' > temp/SPY.txt
+
+echo "CURLing QQQ"
+curl -fsS \
+  -A 'Mozilla/5.0 (X11; Linux x86_64; rv:136.0) Gecko/20100101 Firefox/136.0' \
+  -H 'Accept: application/json,text/plain,*/*' \
+  "https://financialmodelingprep.com/stable/quote-short?symbol=QQQ&apikey=$APIKEY" \
+  | jq -r '.[0].price | floor' > temp/QQQ.txt
+
+echo "CURLing DIA"
+curl -fsS \
+  -A 'Mozilla/5.0 (X11; Linux x86_64; rv:136.0) Gecko/20100101 Firefox/136.0' \
+  -H 'Accept: application/json,text/plain,*/*' \
+  "https://financialmodelingprep.com/stable/quote-short?symbol=DIA&apikey=$APIKEY" \
+  | jq -r '.[0].price | floor' > temp/DIA.txt
 
  # qqq nasdaq
  # dia dow
@@ -25,3 +41,7 @@ curl -fsS \
 
 curl -fsS 'https://api.exchange.coinbase.com/products/BTC-USD/ticker' \
   | jq -r '.price | tonumber | floor' > temp/BTC.txt
+
+# printf "NASDAQ: $%s | SPY: $%s | BTC: $%s" "$(cat temp/QQQ.txt)" "$(cat temp/SPY.txt)" "$(cat temp/BTC.txt)" > temp/ticker.txt
+
+printf "SPY: $%s | BTC: $%s" "$(cat temp/SPY.txt)" "$(cat temp/BTC.txt)" > temp/ticker.txt

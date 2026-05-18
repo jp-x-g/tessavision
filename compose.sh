1a77b3c0fb7beba7fe2e7e0fcbeb6f548ca12216
jp-x-g <jpxg-dev@protonmail.com>
2026-05-18 05:55:21 -0700

    update

--- compose.sh
+++ compose.sh
@@ -18,9 +18,76 @@ cat temp/0date.txt temp/ticker.txt > temp/0right.txt
 #paste blocks/logo3.txt blocks/1x11.txt blocks/barx11.txt blocks/0date.txt > blocks/header.txt
 paste blocks/logo3.txt temp/0right.txt > temp/header.txt
 # 69 plus date = 51 for it
+
+W=58
+H=8
+
+fit_block() {
+  # stdin -> exactly H lines, each exactly W chars
+  # folds long lines, truncates after H, pads width and height
+  awk -v W="$W" -v H="$H" '
+    {
+      line = $0
+      while (length(line) > W) {
+        print substr(line, 1, W)
+        n++
+        if (n >= H) exit
+        line = substr(line, W + 1)
+      }
+      print substr(line, 1, W)
+      n++
+      if (n >= H) exit
+    }
+    END {
+      while (n < H) {
+        print ""
+        n++
+      }
+    }
+  ' | awk -v W="$W" '{ printf "%-*.*s\n", W, W, $0 }'
+}
+
+frame_block() {
+  local title="$1"
+
+  # title line: 27 ═, space title space, 27 ═ = 58 inner chars for "1F"
+  printf '╔%s %s %s╗\n' \
+    "$(printf '═%.0s' {1..27})" \
+    "$title" \
+    "$(printf '═%.0s' {1..27})"
+
+  fit_block | sed 's/^/║/; s/$/║/'
+
+  printf '╚%s╝\n' "$(printf '═%.0s' $(seq 1 "$W"))"
+}
+
+frame_block 1F < temp/1f.txt > temp/1f.box
+frame_block 2F < temp/2f.txt > temp/2f.box
+frame_block 3F < temp/3f.txt > temp/3f.box
+frame_block 4F < temp/4f.txt > temp/4f.box
+
+echo "asdf"
+paste temp/1f.box temp/2f.box >> temp/header.txt
+paste temp/3f.box temp/4f.box >> temp/header.txt
+
+
+
+
+
+
+
+
+
+
 clear
+echo "asdf"
 cat temp/header.txt
 printf '%*s\n' 120 '' | tr ' ' '_'
+
+
+
+
+
 
 #sleep 15
 
